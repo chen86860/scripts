f24d41b79181392951c4746aa522b3824bac11cd
Jack <chen86860@users.noreply.github.com>
2023-08-26 16:05:47 +0000

    fix: bug

--- QuantumultX/weibo_hot_search.js
+++ QuantumultX/weibo_hot_search.js
@@ -1,10 +1,10 @@
 var obj = JSON.parse($response.body);
 
 const myData = {
-  "number": 0,
+  "number": 1,
   "itemid": 1,
   "pic_id": 1,
-  "pic": "",
+  "pic": "https://h5.sinaimg.cn/upload/100/1378/2023/05/16/ic_discovery_hot.png",
   "scheme": "weibointernational://search?keyword=%E6%B8%B8%E9%BE%99%E7%9C%9F%E5%B8%85%EF%BC%81",
   "title": "游龙真帅！",
   "subject_label": "综艺",
