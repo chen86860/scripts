930b55ff12233745dfba4b3e8394472bf3560ddb
Jack <chen86860@users.noreply.github.com>
2023-08-26 15:59:50 +0000

    feat: update

--- QuantumultX/weibo_hot_search.js
+++ QuantumultX/weibo_hot_search.js
@@ -2,13 +2,15 @@ var obj = JSON.parse($response.body);
 
 const myData = {
   "number": 0,
-  "itemid": 0,
+  "itemid": 1,
+  "pic_id": 1,
+  "pic": "",
   "scheme": "weibointernational://search?keyword=%E6%B8%B8%E9%BE%99%E7%9C%9F%E5%B8%85%EF%BC%81",
-  "pic_id": 0,
-  "pic": "https://h5.sinaimg.cn/upload/100/1196/2020/04/23/ic_top.png",
   "title": "游龙真帅！",
-  "type": "searchtop"
-}
+  "subject_label": "综艺",
+  "type": "topic",
+  "icon": "https://h5.sinaimg.cn/upload/100/1196/2020/04/23/ic_top.png"
+};
 
 obj.data = [myData].concat(obj.data.slice(1));
 
