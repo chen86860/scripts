e6736a1c2ad7aab602dde9499163f7415babda26
chen86860 <8399882+chen86860@users.noreply.github.com>
2024-02-06 00:02:30 +0800

    更新 QuantumultX/jike_sponser.js 和 QuantumultX/weibo_hot_search.js 文件

--- QuantumultX/weibo_hot_search.js
+++ QuantumultX/weibo_hot_search.js
@@ -1,17 +1,17 @@
-var obj = JSON.parse($response.body);
+var originBody = JSON.parse($response.body);
 
 const myData = {
-  "number": 1,
-  "itemid": 1,
-  "pic_id": 1,
-  "pic": "https://h5.sinaimg.cn/upload/100/1378/2023/05/16/ic_discovery_hot.png",
-  "scheme": "weibointernational://search?keyword=%E6%B8%B8%E9%BE%99%E7%9C%9F%E5%B8%85%EF%BC%81",
-  "title": "游龙真帅！",
-  "subject_label": "综艺",
-  "type": "topic",
-  "icon": "https://h5.sinaimg.cn/upload/100/1378/2023/05/16/ic_discovery_hot.png"
+  number: 1,
+  itemid: 1,
+  pic_id: 1,
+  pic: 'https://h5.sinaimg.cn/upload/100/1378/2023/05/16/ic_discovery_hot.png',
+  scheme: 'weibointernational://search?keyword=%E6%B8%B8%E9%BE%99%E7%9C%9F%E5%B8%85%EF%BC%81',
+  title: '游龙真帅！',
+  subject_label: '综艺',
+  type: 'topic',
+  icon: 'https://h5.sinaimg.cn/upload/100/1378/2023/05/16/ic_discovery_hot.png',
 };
 
-obj.data = [myData].concat(obj.data.slice(1));
+originBody.data = [myData].concat(originBody.data.slice(1));
 
-$done({ body: JSON.stringify(obj) }); 
+$done({ body: JSON.stringify(originBody) });
